7e8f592ce6d5f534e0be71abb7685ab26b9fcb3a
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2020-04-01 23:00:57 +0300

    Trying create method for generation Cayley Table

--- layered_graph.py
+++ layered_graph.py
@@ -1,18 +1,20 @@
+from cayley_table import CayleyTable
 from graph import Graph
 
 
 class LayeredGraph(Graph):
-    def __init__(self, layers, origin_nodes, origin_arcs):
+    def __init__(self, layers: int, origin_nodes: int, origin_arcs: int):
         super.__init__()
-        # self.layers = layers - нужна ли вообще информация о слоях?
+        self.layers = layers
         self.origin_nodes = origin_nodes
         self.origin_arcs = origin_arcs
 
-    def get_origin_node_index(self, node_index):
+    def get_origin_node_index(self, node_index: int) -> int:
         return node_index % self.origin_nodes
 
-    def get_origin_arc_id(self, arc_index):
+    def get_origin_arc_id(self, arc_index: int) -> int:
         return arc_index % self.origin_arcs
+
 
 def create_layered_graph(graph: Graph, table: CayleyTable) -> LayeredGraph:
     layered_graph = LayeredGraph()
