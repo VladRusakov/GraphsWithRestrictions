2b7a1ef4ebb1f140c59c8c6cd8c5cae50ab55199
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2020-04-15 23:40:28 +0300

    improving layered graph

--- graph_models/networkx_based/layered_graph.py
+++ graph_models/networkx_based/layered_graph.py
@@ -1,13 +1,14 @@
-from typing import Collection
+from typing import Collection, List
 from networkx import MultiDiGraph
-
+import networkx as nx
+nx.shortest_path()
 
 class LayeredGraph(MultiDiGraph):
     def __init__(self, graph: MultiDiGraph, layers: int):
         super().__init__(graph)
         self._layers = layers
         self._origin_nodes = list(graph.nodes)
-        self._max_node = max(self.origin_nodes)
+        self._max_node = max(self.origin_nodes) + 1
 
     @property
     def layers(self):
@@ -22,22 +23,35 @@ class LayeredGraph(MultiDiGraph):
         return self._max_node
 
     def origin_node_index(self, node_index: int) -> int:
+        if node_index not in self.nodes:
+            raise ValueError('Node index should be integer and belong to graph')
         return node_index % self.max_node
 
     def get_node_layer(self, node_index: int) -> int:
+        if node_index not in self.nodes:
+            raise ValueError('Node index should be integer and belong to graph')
         return node_index // self.max_node
 
     def get_layer_nodes(self, layer: int) -> Collection[int]:
-        for node_index in range(self.origin_nodes * layer, self.origin_nodes * (layer+1)):
-            yield node_index
+        if type(layer) != 'int' or layer < 0 or layer >= self.layers:
+            raise ValueError('layer should be integer in range[0;layers)')
+        layer_offset = layer * self.max_node
+        for origin_node_index in self.origin_nodes:
+            yield origin_node_index + layer_offset
+
+    def get_connected_nodes(self, node_index: int) -> List[int]:
+        if node_index not in self.nodes:
+            raise ValueError('Node index should be integer and belong to graph')
+        origin_node = node_index % self.max_node
+        for layer in range(self.layers):
+            yield origin_node + layer * self.max_node
 
     def get_origin_path(self, path_on_layered: Collection[int]) -> Collection[int]:
         pass
-        #return (arc[] for )
 
 
 if __name__ == "__main__":
     import networkx as nx
     g = nx.complete_graph(3)
     lg = LayeredGraph(g, 2)
-    print(lg.origin_nodes)
+    print(lg.get_node_layer(12))
