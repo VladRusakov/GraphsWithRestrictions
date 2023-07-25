b1118ac436bcd0cd60c1e826264c3cee8791b024
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2023-07-25 23:56:05 +0300

    sketch algorithm interfaces

---
+++ flow_algorithms/max_flow_bounded_arcs.py
@@ -0,0 +1,13 @@
+from networkx import MultiDiGraph
+
+
+def calculate_multiplicity(network: MultiDiGraph, source, sink, bounded_arcs: set) -> dict:
+    pass
+
+
+def calculate_max_simple_capacity(network: MultiDiGraph, source, sink, bounded_arcs: set) -> dict:
+    pass
+
+
+def max_flow_in_parallel_network(network: MultiDiGraph, source, sink, bounded_arcs: set, shared_capacity: int) -> float:
+    pass
