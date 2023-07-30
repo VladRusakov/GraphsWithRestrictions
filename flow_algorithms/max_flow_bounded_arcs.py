65b9d30d3b60947ef463ed106c351221c55af952
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2023-07-30 23:59:49 +0300

    bugfix calculation simple capacity;

--- flow_algorithms/max_flow_bounded_arcs.py
+++ flow_algorithms/max_flow_bounded_arcs.py
@@ -14,7 +14,7 @@ def calculate_multiplicity(start_path_arc, network: MultiDiGraph, sink, bounded_
 
 
 def calculate_simple_capacity(start_path_arc, network: MultiDiGraph, sink, bounded_arcs: set) -> float:
-    capacity = 0 if start_path_arc in bounded_arcs else network.get_edge_data(*start_path_arc)[0]["capacity"]
+    capacity = float("inf") if start_path_arc in bounded_arcs else network.get_edge_data(*start_path_arc)[0]["capacity"]
     current_arc = start_path_arc
     while current_arc[1] is not sink:
         arc_end = current_arc[1]
@@ -45,8 +45,6 @@ def max_flow_in_parallel_network(network: MultiDiGraph, source, sink, bounded_ar
     paths_capacities = {path: 0 for path in paths_starts}
     paths_ordering = get_paths_order(paths_multiplicities, paths_simple_capacities)
     for path in paths_ordering:
-        if residual_shared_capacity == 0:
-            break
         if paths_multiplicities[path] == 0:
             paths_capacities[path] = paths_simple_capacities[path]
             continue
@@ -57,7 +55,45 @@ def max_flow_in_parallel_network(network: MultiDiGraph, source, sink, bounded_ar
             residual_shared_capacity -= paths_simple_capacities[path] * paths_multiplicities[path]
         else:
             paths_capacities[path] = path_shared_capacity
-            residual_shared_capacity = 0
+            break
+
+    return sum(paths_capacities.values())
+
+
+def calculate_multiplicity_reversed(end_path_arc, network: MultiDiGraph, source, bounded_arcs: set) -> int:
+    multiplicity = 1 if end_path_arc in bounded_arcs else 0
+    current_arc = end_path_arc
+    while current_arc[0] is not source:
+        arc_start = current_arc[0]
+        current_arc = (list(network.predecessors(arc_start))[0], arc_start)
+        multiplicity += 1 if current_arc in bounded_arcs else 0
+    return multiplicity
+
+
+def get_path_capacity(network, path_end, source, bounded_arcs, residual_shared_capacity: float) -> float:
+    return 0
+
+
+def max_flow_in_tree_structured_network(network, source, sink, bounded_arcs, shared_capacity: float) -> float:
+    paths_ends: List[Tuple] = list(network.in_edges(sink))
+    paths_multiplicities = {path_end: calculate_multiplicity_reversed(path_end, network, source, bounded_arcs)
+                            for path_end in paths_ends}
+
+    for src, dst, attrs in network.edges(data=True):
+        if (src, dst) not in bounded_arcs:
+            attrs["residual_capacity"] = attrs["capacity"]
+
+    residual_shared_capacity = shared_capacity
+
+    paths_capacities = {path_end: 0 for path_end in paths_ends}
+    paths_ordering = get_paths_order(paths_multiplicities, {path_end: 0 for path_end in paths_ends})
+    for path_end in paths_ordering:
+        paths_capacities[path_end] = get_path_capacity(network=network,
+                                                       path_end=path_end,
+                                                       source=source,
+                                                       bounded_arcs=bounded_arcs,
+                                                       residual_shared_capacity=residual_shared_capacity,
+                                                       )
 
     return sum(paths_capacities.values())
 
@@ -71,7 +107,14 @@ if __name__ == '__main__':
         (1, 12, dict(capacity=1)), (12, 4, dict(capacity=1)),
         (1, 4),
     ])
-    print(max_flow_in_parallel_network(network=parallel_network,
+    # print(max_flow_in_parallel_network(network=parallel_network,
+    #                                    source=1,
+    #                                    sink=4,
+    #                                    bounded_arcs={(1, 2), (5, 6), (8, 9), (9, 10), (11, 4), (1, 4)},
+    #                                    shared_capacity=20)
+    #       )
+
+    print(max_flow_in_tree_structured_network(network=parallel_network,
                                        source=1,
                                        sink=4,
                                        bounded_arcs={(1, 2), (5, 6), (8, 9), (9, 10), (11, 4), (1, 4)},
